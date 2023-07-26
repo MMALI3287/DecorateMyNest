656daa9724165fd526d30bfa2d1f80ebf98293f2
MMALI3287 <erfanali3287@gmail.com>
2023-07-26 07:11:13 +0600

    first commit

---
+++ src/DecorateMyNest/DecorateMyNest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DecorateMyNest;
+
+/**
+ *
+ * @author Musaddique Ali
+ */
+public class DecorateMyNest {
+    public static void main(String[] args) {
+        Welcome welcome=new Welcome();
+        welcome.show();
+    }
+}
